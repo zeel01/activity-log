cd3245ce9ed63b36fee722966a5957b862565d78
zeel <zeel02@gmail.com>
2022-01-23 18:43:02 -0500

    Improved the activity template

--- resources/views/activity.blade.php
+++ resources/views/activity.blade.php
@@ -2,15 +2,27 @@
 	<x-slot name="title">
 		{{ $activity->name }}
 	</x-slot>
-	<form method="POST" name="activity">
+	<form method="POST" name="activity" class="activity">
 		@csrf
 		@method('PUT')
 
-		<input type="text" name="name" value="{{ $activity->name }}">
-		<input type="datetime" name="start_time" value="{{ $activity->start_time }}">
-		<input type="datetime" name="end_time" value="{{ $activity->end_time }}">
+		<div class="name">
+			<label for="name">Name</label>
+			<input type="text" name="name" value="{{ $activity->name }}">
+		</div>
+		<div class="time start">
+			<label for="time">Start</label>
+			<input type="datetime" name="start_time" value="{{ $activity->start_time }}">
+		</div>
+		<div class="time end">
+			<label for="time">End</label>
+			<input type="datetime" name="end_time" value="{{ $activity->end_time }}">
+		</div>
 		
-		<textarea name="description" id="" cols="30" rows="10">{{ $activity->description }}</textarea>
+		<div class="description">
+			<label for="description">Description</label>
+			<textarea name="description" id="" cols="30" rows="10">{{ $activity->description }}</textarea>
+		</div>
 
 		<button name="submit" type="submit">Save</button>
 		<button 
